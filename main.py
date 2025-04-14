c586efe926a2cac26ea593e090288e605b595282
David Cuadrado <dav.cuadrado@gmail.com>
2025-04-14 22:56:24 +0200

    feature: file path as argument for app usage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words
+from stats import get_num_words, character_count, print_report
+import sys
 
 # Function to read and return the contents of a text file
 def get_book_text(filepath):
@@ -9,11 +10,17 @@ def text_word_count(text):
     words = text.split()
     return len(words)
 
-# Main function that reads and prints the contents of Frankenstein
 def main():
-    book_text = get_book_text('books/frankenstein.txt')
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path-to-text-file>")
+        return
+    text_path = sys.argv[1]
+    book_text = get_book_text(text_path)
     num_words = get_num_words(book_text)
     print(f'{num_words} words found in the document')
+    character_recount = character_count(book_text)
+    print(character_recount)
+    print_report(book_text, text_path)
 
 # Call main to run the script
 if __name__ == '__main__':
--- stats.py
+++ stats.py
@@ -1,3 +1,34 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def character_count(text):
+    character_recount = {}
+    for character in text.lower():
+        character_recount[character] = character_recount.get(character, 0) + 1
+    return character_recount
+
+def sort_character_counts(char_counts):
+    # Create a list of dictionaries from the char_counts dict
+    sorted_list = [
+        {'character': char, 'count': count}
+        for char, count in char_counts.items()
+    ]
+
+    # Sort by count, from greatest to least
+    sorted_list.sort(key=lambda x: x['count'], reverse=True)
+
+    return sorted_list
+
+def print_report(text, text_path):
+    num_words = get_num_words(text)
+    sorted_list = sort_character_counts(character_count(text))
+    print(f'============ BOOKBOT ============'
+          f'\nAnalyzing book found at {text_path}'
+          f'\n----------- Word Count ----------'
+          f'\n Found {num_words} total words'
+          f'\n--------- Character Count -------')
+    for char_dict in sorted_list:
+        if char_dict["character"].isalpha():
+            print(f'{char_dict["character"]}: {char_dict["count"]}')
+    print('============= END ===============')
